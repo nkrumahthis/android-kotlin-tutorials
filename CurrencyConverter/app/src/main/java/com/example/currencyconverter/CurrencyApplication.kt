c5d1a6afd368e81915b6e9adb87f30255041f989
nkrumahinc <nkrumahthis@gmail.com>
2021-06-03 13:43:39 +0000

    application

--- CurrencyConverter/app/src/main/java/com/example/currencyconverter/CurrencyApplication.kt
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/CurrencyApplication.kt
@@ -1,4 +1,8 @@
 package com.example.currencyconverter
 
-class CurrencyApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyApplication: Application() {
 }
